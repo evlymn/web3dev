2ffdd965207146cef5d561fb6dfd392d53de048b
Evelyn Mendes <evlymn@gmail.com>
2024-06-05 19:28:33 -0300

    vai tudo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getStorage, provideStorage } from '@angular/fire/storage';
+import { WINDOW, windowProvider } from './providers/window';
+import { DOCUMENT } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(),
-    provideFirebaseApp(() =>
-      initializeApp({"projectId":"compartilhagram-com",
-        "appId":"1:995313668366:web:bdd7c22f1ad366581f075f",
-        "databaseURL":"https://compartilhagram-com-default-rtdb.firebaseio.com",
-        "storageBucket":"compartilhagram-com.appspot.com",
-
-        "apiKey":"AIzaSyAd2mX0UDN8pEwmJMRpL7V6OYWo_0grRgw",
-        "authDomain":"compartilhagram-com.firebaseapp.com",
-        "messagingSenderId":"995313668366","measurementId":"G-JDCRMJ416M"})),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: WINDOW,
+      useFactory: (document: Document) => windowProvider(document),
+      deps: [DOCUMENT],
+    },
+  ]
 };
